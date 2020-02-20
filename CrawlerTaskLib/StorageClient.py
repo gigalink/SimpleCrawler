ca450811082c9fc1a680f642e4d981dd5b26754c
林斐 <linfei@gridsum.com>
2020-02-20 20:13:55 +0800

    保存网页原始文件，以后可以重用来解析

--- CrawlerTaskLib/StorageClient.py
+++ CrawlerTaskLib/StorageClient.py
@@ -1,5 +1,6 @@
 import csv
 import datetime
+import os
 from CrawlerPage import Page
 
 class StorageClient:
@@ -7,12 +8,21 @@ class StorageClient:
         # schemas是爬取相关的所有页面类型设置
         # store_schemas是要保存的页面类型
         now = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        self.instanceid = now
+        # 这些用于保存原始html数据
+        if not os.path.exists("CrawledData/Source/{0}".format(now)):
+            os.makedirs("CrawledData/Source/{0}".format(now))
+        self.sourcemappingfile = open("CrawledData/Source/{0}/0mapping.csv".format(now), "a", newline="", encoding="utf-8")
+        self.sourcemappingwriter = csv.writer(self.sourcemappingfile)
+        self.sourcemappingwriter.writerow(["origin url", "filename"])
+        self.pagecount = 0 # 该实例累积保存的页面数
+        # 这些用于保存解析后的结果
         self.datafiles = {}
         self.datafilewriters = {}
         self.schemascolumns = {}
         self.store_schemas = store_schemas
         for schema in store_schemas:
-            self.datafiles[schema] = open("{0}{1}.csv".format(schema, now), "a", newline="", encoding="utf-8")
+            self.datafiles[schema] = open("CrawledData/Extracted/{0}{1}.csv".format(schema, now), "a", newline="", encoding="utf-8")
             self.datafilewriters[schema] = csv.writer(self.datafiles[schema])
             columns = [field["field_name"] for field in schemas[schema]]
             self.datafilewriters[schema].writerow(["url"] + columns)
@@ -22,10 +32,19 @@ class StorageClient:
         return self
 
     def __exit__(self, type, value, trace):
+        self.sourcemappingfile.close()
         for key in self.datafiles:
             self.datafiles[key].close()
     
     def send(self, page:Page):
+        # 爬取到的页面源文件都需要存起来
+        self.pagecount += 1
+        sourcefilename = "{0}.html".format(self.pagecount)
+        f = open("CrawledData/Source/{0}/{1}".format(self.instanceid, sourcefilename), "wt", encoding="utf-8")
+        f.write(page.html_text)
+        f.close()
+        self.sourcemappingwriter.writerow([page.url, sourcefilename])
+        # 解析后的数据按需保存
         if page.schema_name in self.store_schemas:
             row = [page.url]
             row += [page.data.get(columnname)for columnname in self.schemascolumns[page.schema_name]]
