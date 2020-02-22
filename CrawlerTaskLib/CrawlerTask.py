8253c6ce4ee5530cc8dd22355e4e5a43be5deb99
林斐 <linfei@gridsum.com>
2020-02-22 11:04:47 +0800

    当任务启动时保存任务配置，以便以后重跑错误项和重抽信息。另外，启动任务的语句不再放在该文件中，以免改动启动参数的试验被提交到源码管理中。

--- CrawlerTaskLib/CrawlerTask.py
+++ CrawlerTaskLib/CrawlerTask.py
@@ -4,6 +4,7 @@ from Logger import Logger
 from StorageClient import StorageClient
 import json
 import datetime
+import os
 
 class DeepFirstCrawler:
     def __init__(self, browser:VirtualBrowser):
@@ -37,6 +38,22 @@ class FileDataLoader(DataLoader):
         return text
 
 def StartTask(schema_file:str, start_url:str, start_page_schema:str, valve_name:str, valve_param:list, store_schemas:list, taskid:str=None):
+    if taskid is None: 
+        taskid = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    # 用文件记下当前任务的各项参数
+    taskconfig = {}
+    taskconfig["schema_file"] = schema_file
+    taskconfig["start_url"] = start_url
+    taskconfig["start_page_schema"] = start_page_schema
+    taskconfig["valve_name"] = valve_name
+    taskconfig["valve_param"] = valve_param
+    taskconfig["store_schemas"] = store_schemas
+    taskconfigstr = json.dumps(taskconfig)
+    if not os.path.exists("Tasks/{0}".format(taskid)):
+        os.makedirs("Tasks/{0}".format(taskid))
+    f = open("Tasks/{0}/taskconfig.json".format(taskid), "w", encoding="utf-8")
+    f.write(taskconfigstr)
+    f.close()
     # 初始化该任务的数据抽取器
     f = open(schema_file, encoding="utf-8")
     text = f.read()
@@ -51,8 +68,6 @@ def StartTask(schema_file:str, start_url:str, start_page_schema:str, valve_name:
     crawler = DeepFirstCrawler(browser)
     valve = Valve.getvalvebyname(valve_name, valve_param)
 
-    if taskid is None: 
-        taskid = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     with StorageClient(taskid, config["schema"], store_schemas) as storage:
         with Logger(taskid) as logger:
             for page in crawler.getcrawledpages(start_url, None, start_page_schema):
@@ -66,11 +81,3 @@ def StartTask(schema_file:str, start_url:str, start_page_schema:str, valve_name:
                     logger.error(page.url, page.schema_name, str(page.error_msg))
             logger.info("Task finished succesfully.")
 
-
-# filter = [{'schema_name':'nist_article_page', 'field_name': 'PageCreateTime', 'compare_type': '<', 'compare_value': datetime.datetime(2020, 2, 10, 11, 16, 19)}]
-filter = []
-StartTask("CrawlerTaskLib\\NISTSchema.json","TestData\\ArticleSample2.html", "nist_article_page", "FieldValueValve", filter, ["nist_article_page"])
-# StartTask("CrawlerTaskLib\\NISTSchema.json","TestData\\ListPage2012.html", "nist_list_page", "FieldValueValve", filter, ["nist_article_page","nist_list_page"])
-# StartTask("CrawlerTaskLib\\NISTSchema.json","https://www.nist.gov/publications/search", "nist_list_page", "FieldValueValve", filter, ["nist_article_page","nist_list_page"])
-# StartTask("CrawlerTaskLib\\NISTSchema.json","http://localhost/publications/search", "nist_list_page", "FieldValueValve", filter, ["nist_article_page","nist_list_page"])
-# StartTask("CrawlerTaskLib\\NISTSchema.json","https://www.nist.gov/publications/search?k=&t=&a=&s=All&n=&d%5Bmin%5D=&d%5Bmax%5D=&page=2012", "nist_list_page", "FieldValueValve", filter, ["nist_article_page","nist_list_page"])
